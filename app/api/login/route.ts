77e79ea4e593d30224204a67800c567f28e2f3a8
Akash Bodhale <abodhale@Mac.ht.home>
2025-05-03 22:48:58 -0500

    added error msg

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -48,7 +48,7 @@ export async function POST(request:NextRequest){
     catch(error: unknown)
     {
         const errMsg = error instanceof Error ? error.message : "Internal server error";
-        return NextResponse.json({error:error},{status:500})
+        return NextResponse.json({ error: errMsg }, { status: 500 });
     }
 
 }
