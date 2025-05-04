559d5c987ba9cebc0ca78b9190091d3a86c64758
Akash Bodhale <abodhale@Mac.ht.home>
2025-05-04 00:02:48 -0500

    remove nav bar from page tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Bar from "./Bar";
+
 import Login from "./login/page";
 export default function Home() {
   return (
     <div >
-        <Bar />
+        
         <Login />
     </div>
   );
