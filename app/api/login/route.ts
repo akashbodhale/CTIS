0013581e114670fda94f59b4fcd3c9166d6bf9b2
Akash Bodhale <abodhale@Mac.ht.home>
2025-05-03 22:49:12 -0500

    added error msg

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -41,7 +41,7 @@ export async function POST(request:NextRequest){
         //create token
         const token = await jwt.sign(tokenData,process.env.TOKEN_SECRET!,{expiresIn:"1d"})
 
-        const response=NextResponse.json({message: "Login Sucessfull", success:true})
+        const response=NextResponse.json({message: "Login Sucessful", success:true})
         response.cookies.set("token",token,{httpOnly:true})
         return response
     }
